e719b064083db223b03602d94ea88b8ed18ae014
FabiusP <fabius@mail.de>
2026-03-18 18:28:29 +0100

    Update co2.py
    changed dp path

--- co2.py
+++ co2.py
@@ -10,7 +10,7 @@ import pandas as pd
 import streamlit as st
 
 BASE_DIR = Path(__file__).resolve().parent
-DB_PATH = BASE_DIR / "flights_database.db"
+DB_PATH = BASE_DIR / "data" / "flights_database.db"
 
 """
 CO2 module Flights Dashboard
